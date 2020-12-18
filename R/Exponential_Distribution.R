7f464e55fde9702fc5e1e141425d7dbbeda5c78b
Chiara Tumminelli <chiara.tumminelli@edu.unito.it>
2020-12-18 20:52:19 +0100

    Add files via upload

---
+++ R/Exponential_Distribution.R
@@ -0,0 +1,13 @@
+#Restituisce la densita'
+dexp(d, rate)
+
+#Restituisce la funzione distribuzione
+pexp(p, rate)
+
+#Restituisce la funzione quantile
+qexp(q, rate)
+
+#Genera deviazioni casuali
+rexp(r, rate)
+
+#Se rate non e' specificato, assume di default valore 1
---
+++ R/Normal_Distribution.R
@@ -0,0 +1,15 @@
+#Variabile aleatoria normale standard (0,1) 
+#Restituisce la densita'
+dnorm(d, mean, sd)
+
+#Restituisce la funzione distribuzione
+pnorm(p, mean, sd)
+
+#restituisce la funzione quantile
+qnorm(q, mean, sd)
+
+#Restituisce deviazioni casuali
+rnorm(r, mean, sd)
+
+#Se mean oppure sd non sono specificati, assumono
+#rispettivamente i valori 0 e 1.
