91e6ecb72b705254b3689d478abe704128de2580
Chiara Tumminelli <chiara.tumminelli@edu.unito.it>
2020-12-18 21:36:12 +0100

    Add files via upload

---
+++ R/Autovalutazione_3.R
@@ -0,0 +1,50 @@
+#Esercizio 1
+1-pexp(2.2,1.5)
+pexp(1.73,1.5)-pexp(0,1.5)
+1/1.5
+
+#Esercizio 2
+pnorm(4.1,3.8,0.3)
+pnorm(4.25,3.8,0.3)-(pnorm(3.95,3.8,0.3))
+qnorm(0.80,3.8,0.3)
+
+#Esercizio 3
+1-pnorm(0,-2,sqrt(4))
+
+#Esercizio 4
+6*100
+600^2
+
+#Esercizio 5
+(300*5)+250
+
+#Esercizio 6
+
+#Esercizio 7
+
+#Esercizio 8
+
+#Esercizio 9
+pnorm(60,80,sqrt(71))
+
+60*4
+
+
+#Esercizio 10
+
+
+0.60
+
+#Esercizio 11
+
+
+
+8^2
+
+#Esercizio 12
+pnorm(-5,-1,sqrt(9))
+1-pnorm(-1.5,-1,sqrt(9))
+pnorm(-1,-1,sqrt(9))
+pnorm(3,-1,sqrt(9))-pnorm(1,-1,sqrt(9))
+qnorm(0.05,-1,sqrt(9))
+pnorm(1,-1,sqrt(9))
