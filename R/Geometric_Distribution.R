ead5afdd23cf664b14c10a6c38394bf5002b5326
Chiara Tumminelli <chiara.tumminelli@edu.unito.it>
2021-01-04 05:31:30 +0100

    Add files via upload

---
+++ R/Geometric_Distribution.R
@@ -0,0 +1,14 @@
+#Variabile aleatoria geometrica
+#Restituisce la densita'
+dgeom(d, prob)
+
+#Restituisce la funzione distribuzione
+pgeom(d, prob)
+
+#restituisce la funzione quantile
+qgeom(q, prob)
+
+#Restituisce deviazioni casuali
+rgeom(r, prob)
+
+
