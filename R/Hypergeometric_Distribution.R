644f35840c39164b96f95f8a52bafd552677dfe0
Chiara Tumminelli <chiara.tumminelli@edu.unito.it>
2021-01-04 05:37:53 +0100

    Add files via upload

---
+++ R/Hypergeometric_Distribution.R
@@ -0,0 +1,20 @@
+#Variabile aleatoria ipergeometrica
+#Restituisce la densita'
+dhyper(x, m, n, k)
+
+#Restituisce la funzione distribuzione
+phyper(x, m, n, k)
+
+#restituisce la funzione quantile
+qhyper(x, m, n, k)
+
+#Restituisce deviazioni casuali
+rhyper(x, m, n, k)
+
+
+#ESEMPIO
+# x = palline bianche estratte senza reimbussolamento
+# m = totale delle palline bianche nell'urna
+# n = totale palline nere nell'urna
+# k = numero di palline estratte dall'urna 
+
