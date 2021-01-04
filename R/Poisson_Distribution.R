ee5718858ba7bbbc41c4194dadbdb22c0b37fd64
Chiara Tumminelli <chiara.tumminelli@edu.unito.it>
2021-01-04 05:42:03 +0100

    Add files via upload

---
+++ R/Poisson_Distribution.R
@@ -0,0 +1,15 @@
+#Variabile aleatoria di Poisson
+#Restituisce la densita'
+dpois(d, lambda)
+
+#Restituisce la funzione distribuzione
+ppois(p, lambda)
+
+#restituisce la funzione quantile
+qpois(q, lambda)
+
+#Restituisce deviazioni casuali
+rpois(r, lambda)
+
+#Quando va utilizzata e' sempre 
+#specificato nell'esercizio
