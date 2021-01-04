ce4d8b90002fa84e4c43ad7a63ba95705580915c
Chiara Tumminelli <chiara.tumminelli@edu.unito.it>
2021-01-04 05:27:44 +0100

    Add files via upload

---
+++ R/Uniform_Distribution.R
@@ -0,0 +1,15 @@
+#Variabile aleatoria uniforme
+#Restituisce la densita'
+dunif(d, min, max)
+
+#Restituisce la funzione distribuzione
+punif(p, min, max)
+
+#Restituisce la funzione quantile
+qunif(q, min, max)
+
+#Restituisce deviazioni casuali
+runif(r, min, max)
+
+#Se min oppure max non sono specificati, assumono
+#rispettivamente i valori 0 e 1.
