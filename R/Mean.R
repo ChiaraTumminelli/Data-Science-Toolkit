6fbb6378e1ebf865b4684d1f7024b1dccc5f0dc7
Chiara Tumminelli <chiara.tumminelli@edu.unito.it>
2021-01-09 23:49:55 +0100

    Add files via upload

---
+++ R/Mean.R
@@ -0,0 +1,15 @@
+#How to calculate mean
+
+library("UsingR")
+data(babyboom)
+str(babyboom)
+
+#running.time mean
+mean(babyboom$running.time)
+
+#calculate median also
+median(babyboom$running.time)
+
+#in this case median > mean, so it is influenced by sx values
+#if median < mean it is interested by dx values
+#if median = mean ther is simmetry
